e11adf2af4d47c8f4f3b192c0efdc63480e2bfd6
mikevampm323 <mike.vartanyan@yahoo.com>
2020-03-25 00:16:42 -0700

    wrote schemas

--- schema.sql
+++ schema.sql
@@ -1,56 +1,106 @@
-DROP DATABASE IF EXISTS employees_db;
-CREATE DATABASE employees_db;
-USE employees_db;
+DROP DATABASE IF EXISTS employee_db;
 
-CREATE TABLE employee
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+DROP TABLE IF EXISTS department;
+
+CREATE TABLE department
 (
-    id INT NOT NULL
+    id int(11) NOT NULL
     AUTO_INCREMENT,
-  first_name VARCHAR
-    (45) NULL,
-  last_name VARCHAR
-    (45) NULL,
-    role_id INT,
-    manager_id INT,
-   PRIMARY KEY
-    (id),
-  FOREIGN KEY
-    (role_id) REFERENCES employee
-    (id),
-  FOREIGN KEY
-    (manager_id) REFERENCES employee
-    (id)
-  
-  
-);
+  name varchar
+    (30) NOT NULL,
+  PRIMARY KEY
+    (id));
+
+    DROP TABLE IF EXISTS role;
 
     CREATE TABLE role
     (
-        id INT
-            NOT NULL
+        id int(11) NOT NULL
         AUTO_INCREMENT,
-  title VARCHAR
-        (30),
-  salary DECIMAL
-        (10,4),
-        department_id INT,
+  title varchar
+        (30) NOT NULL,
+  salary decimal
+        (10,0) NOT NULL,
+  department_id int
+        (11) NOT NULL,
   PRIMARY KEY
         (id),
-        FOREIGN KEY
-        (department_id) REFERENCES role
-        (id)
-);
+  KEY role_ibfk_1
+        (department_id),
+  CONSTRAINT role_ibfk_1 FOREIGN KEY
+        (department_id) REFERENCES department
+        (id));
 
-        CREATE TABLE department
+        DROP TABLE IF EXISTS employee;
+
+        CREATE TABLE employee
         (
-            id INT
-            AUTO_INCREMENT NOT NULL,
-  name VARCHAR
-            (30),
-            PRIMARY KEY
-            (id)
+            id int(11) NOT NULL
+            AUTO_INCREMENT,
+  first_name varchar
+            (30) NOT NULL,
+  last_name varchar
+            (30) NOT NULL,
+  role_id int
+            (11) NOT NULL,
+  manager_id int
+            (11) DEFAULT NULL,
+  PRIMARY KEY
+            (id),
+  KEY employee_ibfk_1
+            (role_id),
+  KEY employee_ibfk_2
+            (manager_id),
+  CONSTRAINT employee_ibfk_1 FOREIGN KEY
+            (role_id) REFERENCES role
+            (id),
+  CONSTRAINT employee_ibfk_2 FOREIGN KEY
+            (manager_id) REFERENCES role
+            (id));
+
+
+            INSERT INTO department
+                (id, name)
+            VALUES
+                (NULL, "Human Resources"),
+                (NULL, "Operations"),
+                (NULL, "Finance"),
+                (NULL, "Marketing"),
+                (NULL, "Sales"),
+                (NULL, "Information Technology");
+
+            INSERT INTO role
+                (id, department_id, title, salary)
+            VALUES
+                (NULL, 1, "HR Manager", 60000),
+                (NULL, 1, "HR Advisor", 25000),
+                (NULL, 2, "Operations Lead", 70000),
+                (NULL, 2, "Operations Analyst", 30000),
+                (NULL, 3, "Internal Auditor", 70000),
+                (NULL, 3, "Accountant", 50000),
+                (NULL, 4, "Events Officer", 40000),
+                (NULL, 4, "Marketing Assistant", 20000),
+                (NULL, 5, "Sales Manager", 80000),
+                (NULL, 5, "Sales Advisor", 40000),
+                (NULL, 6, "Junior Developer", 25000),
+                (NULL, 6, "Network Engineer", 40000);
+
+            INSERT INTO employee
+                (id, first_name, last_name, role_id, manager_id)
+            VALUES
+                (NULL, "Marvin", "Hernandez", 12, NULL),
+                (NULL, "Jennifer", "Chan", 11, 3),
+                (NULL, "Ora", "Carter", 2, 10),
+                (NULL, "Woodrow", "Stevens", 3, NULL),
+                (NULL, "Owen", "Hampton", 4, 4),
+                (NULL, "Irvin", "Newman", 10, 9),
+                (NULL, "Rafael", "Wheeler", 7, NULL),
+                (NULL, "Jana", "Hanson", 2, 10),
+                (NULL, "Casey", "Huff", 9, NULL),
+                (NULL, "Don", "Floyd", 1, NULL),
+                (NULL, "Terri", "Lloyd", 12, NULL)
   
-);
-
-
-        
---
+++ start.js
@@ -0,0 +1,80 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+    host: "localhost",
+
+    // Your port; if not 3306
+    port: 3306,
+
+    // Your username
+    user: "root",
+
+    // Your password
+    password: "Lakers323",
+    database: "employee_db"
+});
+
+connection.connect(function (err) {
+    if (err) throw err;
+    startApp();
+
+})
+
+
+function startApp() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Employees By Dept",
+                "View All Employees By Manager",
+                "Add Employee",
+                "Remove Employee"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View All Employees":
+                    viewEmployees(); break;
+
+                case "View All Employees By Dept":
+                    employeesDept();
+                    break;
+
+                case "View All Employees By Managere":
+                    //   rangeSearch();
+                    break;
+
+                case "Add Employee":
+                    //   songSearch();
+                    break;
+
+                case "Remove Employee":
+                    //   songAndAlbumSearch();
+                    break;
+            }
+        })
+}
+
+const viewEmployees = () => {
+    connection.query("SELECT employee.id,employee.first_name AS First,employee.last_name AS Last, role.title,role.salary,department.name AS dept FROM employee_db.employee   JOIN employee_db.role JOIN employee_db.department", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        // connection.end();
+    });
+
+}
+
+const employeesDept = () => {
+    connection.query("SELECT * FROM employee", function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        // connection.end();
+    });
+
+}
