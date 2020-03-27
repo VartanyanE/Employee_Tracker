4f2a43ff067d3e0a51349066e5acb30b59b9d6e2
mikevampm323 <mike.vartanyan@yahoo.com>
2020-03-26 19:38:18 -0700

    added new function

--- start.js
+++ start.js
@@ -34,6 +34,9 @@ function startApp() {
                 "View All Employees By Dept",
                 "View All Employees By Manager",
                 "Add Employee",
+                'Update Employee Role',
+                "Add Department",
+
                 "Remove Employee"
             ]
         })
@@ -51,30 +54,166 @@ function startApp() {
                     break;
 
                 case "Add Employee":
-                    //   songSearch();
+                    addEmployee();
+                    break;
+
+                case "Add Department":
+                    addDepartment();
+                    break;
+
+                case "Update Employee Role":
+                    updateEmloyeeRole();
                     break;
 
                 case "Remove Employee":
-                    //   songAndAlbumSearch();
+                    deleteEmployee();
                     break;
             }
         })
 }
 
 const viewEmployees = () => {
-    connection.query("SELECT * FROM employee ", function (err, res) {
+    connection.query("SELECT employee.id,employee.first_name AS First,employee.last_name AS Last,role.title,role.salary,department.name AS Department FROM employee_db.employee LEFT JOIN employee_db.role ON employee.role_id = role.id LEFT JOIN employee_db.department ON role.department_id = department.id ", function (err, res) {
         if (err) throw err;
         console.table(res);
         // connection.end();
+        startApp();
     });
 
 }
 
 const employeesDept = () => {
-    connection.query("SELECT * FROM employee", function (err, res) {
+    connection.query("SELECT employee.id,employee.first_name AS First, employee.last_name AS Name,department.name AS Department FROM employee_db.employee LEFT JOIN employee_db.role ON employee.role_id = role.id LEFT JOIN employee_db.department ON role.department_id = department.id", function (err, res) {
         if (err) throw err;
         console.table(res);
         // connection.end();
+        startApp();
     });
 
+}
+
+function addEmployee() {
+    inquirer
+        .prompt([
+            {
+                name: "first_name",
+                type: "input",
+                message: "What is the new employee's first name?"
+            },
+            {
+                name: "last_name",
+                type: "input",
+                message: "What is the new employee's last name?"
+            },
+            {
+                name: "role",
+                type: "input",
+                message: "What is the new employee's role? (Please enter the role ID)"
+            }
+        ])
+        .then(function (answer) {
+            connection.query(
+                "INSERT INTO employee SET ?",
+                {
+                    first_name: answer.first_name,
+                    last_name: answer.last_name,
+                    role_id: answer.role
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("The new employee has been created successfully!");
+                    startApp();
+                }
+            );
+        });
+
+
+}
+
+const addDepartment = () => {
+    inquirer
+        .prompt([
+            {
+                name: "department",
+                type: "input",
+                message: "What department would you like to add?"
+            },
+
+        ])
+        .then(function (answer) {
+            connection.query(
+                "INSERT INTO department SET ?",
+                {
+                    name: answer.department,
+
+                },
+                function (err) {
+                    if (err) throw err;
+                    console.log("The new department has been created successfully!");
+                    startApp();
+                }
+            );
+        });
+
+
+}
+
+const updateEmloyeeRole = () => {
+    inquirer
+        .prompt([
+            {
+                name: "employee",
+                type: "input",
+                message: "Which employee's role would you like to update? (Enter the employee ID)"
+            },
+
+            {
+                name: "newRole",
+                type: "input",
+                message: "What would you like to set the new role? (Enter Role Id)"
+            },
+
+        ])
+        .then(function (answer) {
+            connection.query(
+                "UPDATE employee SET ? where ?",
+                [
+                    {
+                        role_id: answer.newRole
+                    },
+                    {
+                        id: answer.employee
+                    }
+                ],
+                function (err) {
+                    if (err) throw err;
+                    console.log("The employee's role has been updated successfully!");
+                    startApp();
+                }
+            );
+        });
+}
+
+
+function deleteEmployee() {
+    inquirer
+        .prompt({
+            name: "employee",
+            type: "input",
+            message:
+                "Which employee would you like to delete? (Please enter the employee's ID)"
+        })
+        .then(function (answer) {
+            connection.query(
+                "DELETE from employee where ?",
+                {
+                    id: answer.employee
+                },
+                function (err, result) {
+                    if (err) throw err;
+                    console.log("Employee has been deleted successfully!");
+                    startApp();
+                }
+            );
+        });
 }
